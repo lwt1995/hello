278073121406d41c3e63eccb2fc04595ec3d51d2
lwt1995 <1152925359@qq.com>
2023-03-06 17:10:27 +0800

    update 自定义线程池代码与boost线程池代码分开管理

--- threadPoolFactory/main.cpp
+++ threadPoolFactory/main.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 #include <thread>
 #include<mutex>
-#include <boost/asio.hpp>
+//#include <boost/asio.hpp>
+#include "../threadPoolFactory/threadpool.hpp"
 /* 互斥锁 */
 std::mutex mutex_iostream;
 
@@ -18,26 +19,38 @@ int main(int argc, const char **argv) {
     printf("%d\n", argc);
     printf("%s\n", argv[0]);
     /* 定义一个4线程的线程池 */
-    boost::asio::thread_pool tp(4);
+//    boost::asio::thread_pool tp(4);
+    ThreadPool tp(4);
+    tp.init();
     /* 将函数投放到线程池 */
     for (int i = 0; i < 5; ++i) {
-        boost::asio::post(tp, my_task);
+//        boost::asio::post(tp, my_task);
+        tp.submit(my_task).get();
     }
 
     /* 将语句块投放到线程池 */
     for (int i = 0; i < 5; ++i) {
-        boost::asio::post(
-                tp,
+//        boost::asio::post(
+//                tp,
+//                []() {
+//                    std::lock_guard<std::mutex> lg(mutex_iostream);
+//                    std::cout.flush();
+//                    std::cout << "This is lambda task." << std::endl;
+//                    std::cout.flush();
+//                });
+        tp.submit(
                 []() {
                     std::lock_guard<std::mutex> lg(mutex_iostream);
                     std::cout.flush();
                     std::cout << "This is lambda task." << std::endl;
                     std::cout.flush();
-                });
+                }).get();
+
     }
 
     /* 退出所有线程 */
-    tp.join();
+//    tp.join();
+      tp.shutdown();
 
     //system("PAUSE");
 
